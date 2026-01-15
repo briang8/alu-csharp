e07b5674f91796d0487cfb73802befd49a603d75
briankag8 <b.gatanazi@alustudent.com>
2026-01-15 22:03:48 +0200

    push

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet new console -o 1-new_project
+dotnet new console -o 1-new_project --force > /dev/null 2>&1
 
-dotnet build 1-new_project
+dotnet build 1-new_project | grep -A 2 "Build succeeded"
