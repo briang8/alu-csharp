b51d6d3428df08544b1037a634772d3bfe12a4ad
briankag8 <b.gatanazi@alustudent.com>
2026-02-04 11:49:24 +0200

    tryagain

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Array
+{
+
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            if (i < size - 1)
+                Console.Write(i + " ");
+            else
+                Console.WriteLine(i);
+        }
+
+        return array;
+    }
+}
