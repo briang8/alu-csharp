38ac3f4e1083f026981a4cc91aa7095e21efc6e3
briankag8 <b.gatanazi@alustudent.com>
2026-02-04 10:22:18 +0200

    pushmetal

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,30 +2,33 @@
 
 class Array
 {
-    public static int?[] CreatePrint(int size)
+
+    public static int[] CreatePrint(int size)
     {
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+
         if (size == 0)
         {
             Console.WriteLine();
             Console.WriteLine($"Array Length: {size}");
-            return new int?[0];
+            return new int[0];
         }
 
-        int?[] array = new int?[size];
+        int[] array = new int[size];
+
         for (int i = 0; i < size; i++)
         {
             array[i] = i;
             if (i < size - 1)
                 Console.Write(i + " ");
             else
-                Console.Write(i);
+                Console.WriteLine(i);
         }
-        Console.WriteLine();
+
         Console.WriteLine($"Array Length: {size}");
         return array;
     }
