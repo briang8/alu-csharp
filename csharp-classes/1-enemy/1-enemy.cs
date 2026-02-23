585072921af0f7858748e81397cfb9740b9dd09b
briankag8 <b.gatanazi@alustudent.com>
2026-02-23 20:52:09 +0200

    everyklass

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+       
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
